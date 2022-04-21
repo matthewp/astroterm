fc8f20db0c2bb860482ace0c14757676a9b8ff39
Matthew Phillips <matthew@matthewphillips.info>
2022-04-21 14:47:15 -0400

    New favicons

---
+++ _site/src/components/HeadCommon.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from 'preact';
+
+export default function() {
+    return (
+        <Fragment>
+            <link rel="apple-touch-icon" sizes="180x180" href="/astroterm/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/astroterm/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/astroterm/favicon-16x16.png"/>
+            <link rel="icon" type="image/x-icon" href="/astroterm/favicon.ico"/>
+        </Fragment>
+    )
+}
