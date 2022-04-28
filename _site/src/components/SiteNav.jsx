c5a438844c0dfb6ef0acb9ea1bf1ee2290604b4e
Matthew Phillips <matthew@matthewphillips.info>
2022-04-28 08:17:21 -0400

    Add site navigation links

---
+++ _site/src/components/SiteNav.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from 'preact';
+import '../styles/SiteNav.css';
+
+function SiteLink({ children, link }) {
+    return <li><a href={link}>{children}</a></li>
+}
+
+export default function() {
+    return (
+        <nav class="site-nav">
+            <ul>
+                <SiteLink link="https://github.com/matthewp/astroterm">GitHub</SiteLink>
+            </ul>
+        </nav>
+    );
+}
